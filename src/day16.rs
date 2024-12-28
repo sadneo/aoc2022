facda72f38006cc49a9919be2eaf6ce7525420c9
sadneo <haywardeel@gmail.com>
2024-12-27 21:52:19 -0500

    day 16 but not dynamic

--- src/day16.rs
+++ src/day16.rs
@@ -1,7 +1,16 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 const START: &str = "AA";
 const TURNS: u32 = 30;
+
+// graph of nodes
+// move to nodes and open them or don't open them
+// (prim or dijkstra) + 0/1 knapsack
+//
+// prim's algorithm: start with how many nodes there are
+// HashMap of nodes, contain whether they've been went through before
+// take node, go next
+// to go next, spawn new path for each open/close, handle those spawned paths in a queue
 
 #[derive(Debug, PartialEq)]
 struct Location {
@@ -18,67 +27,47 @@ pub fn run(input: &str) {
             let semicolon_index = line.find(';').unwrap();
             let es_index = line.find("valve").unwrap() + 6;
             let flow_rate = line[23..semicolon_index].parse::<u64>().unwrap();
-            let tunnels: Vec<String> = line[es_index..].split(", ").map(str::trim).map(str::to_owned).collect();
+            let tunnels: Vec<String> = line[es_index..]
+                .split(", ")
+                .map(str::trim)
+                .map(str::to_owned)
+                .collect();
 
             let location = Location { flow_rate, tunnels };
             (key, location)
         })
         .collect();
 
-    let pressure = simulate(&locations, START, 0, &mut 0, &mut vec![], &mut vec![]);
+    let pressure = simulate2(&locations, &HashSet::new(), locations.get(START).unwrap(), TURNS, 0);
     println!("{pressure}");
 }
 
-// check the current location
-// if the current location is closed, then open it and then continue
-// else, for each tunnel in the current location's tunnels, do something
-
-fn simulate<'a>(
-    locations: &'a HashMap<String, Location>,
-    current_name: &str,
-    mut turns_taken: u32,
-    pressure: &mut u64,
-    open_valves: &mut Vec<&'a Location>,
-    path_taken: &mut Vec<&'a Location>,
+fn simulate2(
+    locations: &HashMap<String, Location>,
+    visited_locations: &HashSet<String>,
+    cur_location: &Location,
+    turn: u32,
+    total: u64,
 ) -> u64 {
-    if turns_taken == TURNS {
-        return *pressure;
+    if turn == 0 {
+        return total;
     }
 
-    let flow_rate: u64 = open_valves
-        .iter()
-        .map(|location| location.flow_rate)
-        .sum();
-    turns_taken += 1;
-    *pressure += flow_rate;
+    let mut best_total = 0;
+    for location_name in cur_location.tunnels.iter() {
+        let new_location = locations.get(location_name).unwrap();
+        let mut new_visited = visited_locations.clone();
+        new_visited.insert(location_name.to_owned());
 
-    let current_valve = &locations[current_name];
-    if !open_valves.contains(&current_valve) && current_valve.flow_rate > 0 {
-        open_valves.push(current_valve);
-        simulate(
-            locations,
-            current_name,
-            turns_taken,
-            pressure,
-            open_valves,
-            path_taken,
-        )
-    } else {
-        path_taken.push(current_valve);
-        let mut pressurepressure = 0;
-        for new_name in &current_valve.tunnels {
-            let test_pressure = simulate(
-                locations,
-                new_name,
-                turns_taken,
-                pressure,
-                open_valves,
-                path_taken,
-            );
-            if test_pressure > pressurepressure {
-                pressurepressure = test_pressure
-            }
+        // try each path
+        let result1 = simulate2(locations, &new_visited, new_location, turn - 1, total);
+        best_total = std::cmp::max(best_total, result1);
+
+        if turn >= 2 {
+            let result2 = simulate2(locations, &new_visited, new_location, turn - 2, total + new_location.flow_rate);
+            best_total = std::cmp::max(best_total, result2);
         }
-        *pressure
     }
+
+    best_total
 }
